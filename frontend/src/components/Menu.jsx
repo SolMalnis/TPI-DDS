56cb931a599fe18c0131c146ca4f52c8be89fdab
Eliseo <eliseodavila004@gmail.com>
2024-06-26 12:41:32 -0300

    agrego proveedores al menu

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -3,29 +3,34 @@ import { NavLink } from "react-router-dom";
 
 function Menu(){
     return (
-        <nav className= "navbar navbar-dark bg-dark navbar-expand-md">
-            <a className= "navbar-brand">
+        <nav className="navbar navbar-dark bg-dark navbar-expand-md">
+            <a className="navbar-brand">
                 <i className="fa fa-industry"></i>
                 &nbsp;<i>Ventas</i>
             </a>
-            <button className="navbar-toggler" type="button"  data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
             </button>
             <div className="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul className="navbar-nav mr-auto">
                     <li className="nav-item">
                         <NavLink className="nav-link" to='/Inicio'>
-                        Inicio
+                            Inicio
                         </NavLink>
                     </li>
                     <li className="nav-item">
                         <NavLink className="nav-link" to='/articulos'>
-                        Articulos
+                            Articulos
                         </NavLink>
                     </li>
-                    <li className="nav-item">	
+                    <li className="nav-item">   
                         <NavLink className="nav-link" to="/clientes">
-                        Clientes
+                            Clientes
+                        </NavLink>
+                    </li>
+                    <li className="nav-item">   
+                        <NavLink className="nav-link" to="/proveedores">
+                            Proveedores
                         </NavLink>
                     </li>
                 </ul>
